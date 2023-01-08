027317c85593668b06878fe1762ee57884d335df
FlEtOsAuRoM99 <mariotti.samuele1999@gmail.com>
2023-01-08 19:14:17 +0100

    2nd exercise, create

---
+++ 29gen2020/LinguaggiDinamici-esercizio2-29gen2020.py
@@ -0,0 +1,14 @@
+'''
+Esercizio 2) Definire una classe per rappresentare multi-insiemi, ovvero
+“insiemi” in cui gli elementi possono comparire anche con molteplicità
+maggiore di 1. La classe deve supportare almeno le due operazioni di
+unione e intersezione di multi-insiemi. Le definizioni ricordano quelle
+di mcm e MCD di interi:
+> l’unione consiste degli elementi presenti in uno o entrambi gli in-
+siemi con molteplicità pari al massimo delle due (se l’elemento non
+è presente si può intendere che abbia molteplicità 0);
+> l’intersezione consiste degli elementi presenti in entrambi gli in-
+siemi con molteplicità pari al minimo delle due.
+Le operazioni devono restituire nuovi multi-insiemi, senza modificare
+gli operandi.
+'''
